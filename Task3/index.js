8fe93298f6bbb2ada8de5914dc4eca7336f37a10
MohamedYehia2219 <77175242+MohamedYehia2219@users.noreply.github.com>
2024-02-26 07:22:39 +0200

    mongodb connection

--- Task3/index.js
+++ Task3/index.js
@@ -1,6 +1,11 @@
-const PORT=5000; 
+const PORT=5000;
+const DB_URL="mongodb://localhost/testDB" 
+//server
 const express =require('express');
 const server = express();
-server.listen(PORT,err=>{console.log(err);})
+server.listen(PORT,()=>{console.log(`server is listening on port ${PORT}`);})
 //third party middleware
 server.use(express.json());
+//mongoose
+const mongoose = require('mongoose');
+mongoose.connect(DB_URL).then(()=>{console.log("connected to DB");}).catch((err)=>{console.log(err);})
